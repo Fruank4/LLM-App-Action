ee342c8b510e6d376a2c4b44b92ad5b499ab0267
Chuang Li <657576638@qq.com>
2025-06-04 11:27:44 +0800

    rm`

---
+++ ai-langchain/src/test/java/com/example/ailangchain/AiLangchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ailangchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLangchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
